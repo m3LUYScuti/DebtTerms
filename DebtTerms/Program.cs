e76d31e84dabc233db2d556d5741236d8217d2a8
davit <davit@GOSH>
2024-09-25 17:43:12 +0400

    Add project files.

---
+++ DebtTerms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Sesxis misagebad sheiyvanet tqveni monacemebi");
+Console.WriteLine("Asaki:");
+
+int Asaki = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Wliuri Shemosavali $shi (chaweret mxolod cifrebi)");
+
+int Shemosavali = int.Parse(Console.ReadLine());
+
+if (Asaki <= 17)
+
+    Console.WriteLine("Arasrulwlovanze sesxi ar gaicema");
+
+else if (Shemosavali >= 25000)
+{
+    Console.WriteLine("Sesxi dagimtkicdat");
+}
+else if (Shemosavali <= 25000)
+{
+    Console.WriteLine("Sesxistvis arasakmarisi wliuri Shemosavali");
+    Console.WriteLine("Imistvis rom aigot sesxi sachiroa tanamonawile");
+    Console.WriteLine("Gyavt tu ara tanamonawile? (upasuxed mxolod 'Ki' da 'Ara' ) ");
+    string tanamonawile = Console.ReadLine();
+    if (tanamonawile == "Ki")
+        Console.WriteLine("Tqven shegidzliat TanamonawilesTan ertad aigot sesxi");
+
+    else if (tanamonawile == "Ara")
+        Console.WriteLine("Samwuxarod sesxze uari getqvat");
+}
+else Console.WriteLine("Gtxovt swored sheiyvanot pasuxi");
+
+Console.ReadLine(); 
